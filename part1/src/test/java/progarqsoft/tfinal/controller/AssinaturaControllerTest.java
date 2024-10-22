7310ab75447ef9f825de7a96ea97844eda1a8140
v-Kaefer <128633407+v-Kaefer@users.noreply.github.com>
2024-10-22 13:47:05 -0300

    Build + Testes Funcionais
    - Fix `AssinaturaControllerTest.java`
        * `@Autowired` + `@BeforeEeach` + Imports de `model` e `repository`

--- part1/src/test/java/progarqsoft/tfinal/controller/AssinaturaControllerTest.java
+++ part1/src/test/java/progarqsoft/tfinal/controller/AssinaturaControllerTest.java
@@ -1,5 +1,6 @@
 package progarqsoft.tfinal.controller;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -9,6 +10,10 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import progarqsoft.tfinal.model.Aplicativo;
+import progarqsoft.tfinal.model.Cliente;
+import progarqsoft.tfinal.repository.AplicativoRepository;
+import progarqsoft.tfinal.repository.ClienteRepository;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -18,9 +23,33 @@ public class AssinaturaControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @Autowired
+    private AplicativoRepository aplicativoRepository;
+
+    private Long clienteId;
+    private Long aplicativoId;
+
+    @BeforeEach
+    public void setUp() {
+        Cliente cliente = new Cliente();
+        cliente.setNome("Cliente Teste");
+        cliente.setEmail("cliente@teste.com");
+        cliente = clienteRepository.save(cliente);
+        clienteId = cliente.getCodigo();
+
+        Aplicativo aplicativo = new Aplicativo();
+        aplicativo.setNome("Aplicativo Teste");
+        aplicativo.setCustoMensal(10.0f);
+        aplicativo = aplicativoRepository.save(aplicativo);
+        aplicativoId = aplicativo.getCodigo();
+    }
+
     @Test
     public void criarAssinaturaTest() throws Exception {
-        String jsonRequest = "{\"clienteId\":1,\"aplicativoId\":1}";
+        String jsonRequest = String.format("{\"clienteId\":%d,\"aplicativoId\":%d}", clienteId, aplicativoId);
 
         mockMvc.perform(post("/api/assinaturas")
                 .contentType(MediaType.APPLICATION_JSON)
